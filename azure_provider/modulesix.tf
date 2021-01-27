bd870bf7d3f2031b32ac11c79e694dc4be73ff70
raguirreg10 <romellaguirre@gmail.com>
2021-01-26 23:55:28 -0600

    azure provider example

--- azure_provider/modulesix.tf
+++ azure_provider/modulesix.tf
@@ -1,6 +1,19 @@
-##############################################################################################
+##################################################################################
+# CONFIGURATION (for Terraform > 0.12)
+##################################################################################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+
+    }
+  }
+}
+
+##################################################################################
 # VARIABLES
-##############################################################################################
+##################################################################################
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
@@ -9,22 +22,33 @@ variable "key_name" {}
 variable "region" {
   default = "us-east-1"
 }
+
 variable "network_address_space" {
   default = "10.1.0.0/16"
 }
-variable "subnet1_address_space" {
-  default = "10.1.0.0/24"
-}
-variable "subnet2_address_space" {
-  default = "10.1.1.0/24"
-}
-variable "bucket_name_prefix" {}
+
 variable "billing_code_tag" {}
 variable "environment_tag" {}
+variable "bucket_name_prefix" {}
 
-##############################################################################################
+variable "arm_subscription_id" {}
+variable "arm_principal" {}
+variable "arm_password" {}
+variable "tenant_id" {}
+variable "dns_zone_name" {}
+variable "dns_resource_group" {}
+
+variable "instance_count" {
+  default = 2
+}
+
+variable "subnet_count" {
+  default = 2
+}
+
+##################################################################################
 # PROVIDERS
-##############################################################################################
+##################################################################################
 
 provider "aws" {
   access_key = var.aws_access_key
@@ -32,9 +56,23 @@ provider "aws" {
   region     = var.region
 }
 
-##############################################################################################
-# LOCAL
-##############################################################################################
+provider "azurerm" {
+  subscription_id = var.arm_subscription_id
+  client_id       = var.arm_principal
+  client_secret   = var.arm_password
+  tenant_id       = var.tenant_id
+  alias           = "arm-1"
+  
+  #Added when using service principal with limited permissions
+  skip_provider_registration = true
+
+  #Required for version 2.0 of provider
+  features {}
+}
+
+##################################################################################
+# LOCALS
+##################################################################################
 
 locals {
   common_tags = {
@@ -45,9 +83,9 @@ locals {
   s3_bucket_name = "${var.bucket_name_prefix}-${var.environment_tag}-${random_integer.rand.result}"
 }
 
-##############################################################################################
+##################################################################################
 # DATA
-##############################################################################################
+##################################################################################
 
 data "aws_availability_zones" "available" {}
 
@@ -71,48 +109,43 @@ data "aws_ami" "aws-linux" {
   }
 }
 
-##############################################################################################
+##################################################################################
 # RESOURCES
-##############################################################################################
+##################################################################################
 
-## RANDOM ID ##
+#Random ID
 resource "random_integer" "rand" {
   min = 10000
   max = 99999
 }
 
-## NETWORKING SECTION ##
+# NETWORKING #
 resource "aws_vpc" "vpc" {
   cidr_block = var.network_address_space
 
   tags = merge(local.common_tags, { Name = "${var.environment_tag}-vpc" })
+
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
   tags = merge(local.common_tags, { Name = "${var.environment_tag}-igw" })
+
 }
 
-resource "aws_subnet" "subnet1" {
-  cidr_block              = var.subnet1_address_space
+resource "aws_subnet" "subnet" {
+  count                   = var.subnet_count
+  cidr_block              = cidrsubnet(var.network_address_space, 8, count.index)
   vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet1" })
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet${count.index + 1}" })
+
 }
 
-resource "aws_subnet" "subnet2" {
-  cidr_block              = var.subnet2_address_space
-  vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[1]
-
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-subnet2" })
-}
-
-## ROUTING ##
+# ROUTING #
 resource "aws_route_table" "rtb" {
   vpc_id = aws_vpc.vpc.id
 
@@ -124,17 +157,13 @@ resource "aws_route_table" "rtb" {
   tags = merge(local.common_tags, { Name = "${var.environment_tag}-rtb" })
 }
 
-resource "aws_route_table_association" "rta-subnet-1" {
-  subnet_id      = aws_subnet.subnet1.id
+resource "aws_route_table_association" "rta-subnet" {
+  count          = var.subnet_count
+  subnet_id      = aws_subnet.subnet[count.index].id
   route_table_id = aws_route_table.rtb.id
 }
 
-resource "aws_route_table_association" "rta-subnet-2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.rtb.id
-}
-
-## SECURITY GROUPS ##
+# SECURITY GROUPS #
 resource "aws_security_group" "elb-sg" {
   name   = "nginx_elb_sg"
   vpc_id = aws_vpc.vpc.id
@@ -155,12 +184,12 @@ resource "aws_security_group" "elb-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-elb" })
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-elb-sg" })
 }
 
 # Nginx security group 
 resource "aws_security_group" "nginx-sg" {
-  name   = "nginx_demo"
+  name   = "nginx_sg"
   vpc_id = aws_vpc.vpc.id
 
   # SSH access from anywhere
@@ -179,24 +208,24 @@ resource "aws_security_group" "nginx-sg" {
     cidr_blocks = [var.network_address_space]
   }
 
-  #Outbound internet access
+  # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-sg" })
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-nginx-sg" })
 }
 
 # LOAD BALANCER #
 resource "aws_elb" "web" {
   name = "nginx-elb"
 
-  subnets         = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  subnets         = aws_subnet.subnet[*].id
   security_groups = [aws_security_group.elb-sg.id]
-  instances       = [aws_instance.nginx1.id, aws_instance.nginx2.id]
+  instances       = aws_instance.nginx[*].id
 
   listener {
     instance_port     = 80
@@ -208,24 +237,27 @@ resource "aws_elb" "web" {
   tags = merge(local.common_tags, { Name = "${var.environment_tag}-elb" })
 }
 
-## INSTANCES ##
-resource "aws_instance" "nginx1" {
+# INSTANCES #
+resource "aws_instance" "nginx" {
+  count                  = var.instance_count
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet1.id
-  key_name               = var.key_name
+  subnet_id              = aws_subnet.subnet[count.index % var.subnet_count].id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  key_name               = var.key_name
   iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+  depends_on             = [aws_iam_role_policy.allow_s3_all]
 
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
     private_key = file(var.private_key_path)
+
   }
 
   provisioner "file" {
-    content     = <<EOF
+    content = <<EOF
 access_key =
 secret_key =
 security_token =
@@ -237,7 +269,7 @@ EOF
   }
 
   provisioner "file" {
-    content     = <<EOF
+    content = <<EOF
 /var/log/nginx/*log {
     daily
     rotate 10
@@ -253,6 +285,7 @@ EOF
 }
 
 EOF
+
     destination = "/home/ec2-user/nginx"
   }
 
@@ -269,83 +302,14 @@ EOF
       "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
       "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
       "sudo logrotate -f /etc/logrotate.conf"
-
+      
     ]
   }
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-nginx1" })
+  tags = merge(local.common_tags, { Name = "${var.environment_tag}-nginx${count.index + 1}" })
 }
 
-resource "aws_instance" "nginx2" {
-  ami                    = data.aws_ami.aws-linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet2.id
-  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-  key_name               = var.key_name
-  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file(var.private_key_path)
-
-  }
-
-  provisioner "file" {
-    content     = <<EOF
-access_key =
-secret_key =
-security_token =
-use_https = True
-bucket_location = US
-
-EOF
-    destination = "/home/ec2-user/.s3cfg"
-  }
-
-  provisioner "file" {
-    content     = <<EOF
-/var/log/nginx/*log {
-    daily
-    rotate 10
-    missingok
-    compress
-    sharedscripts
-    postrotate
-    endscript
-    lastaction
-        INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-        sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${aws_s3_bucket.web_bucket.id}/nginx/$INSTANCE_ID/
-    endscript
-}
-
-EOF
-    destination = "/home/ec2-user/nginx"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start",
-      "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
-      "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
-      "sudo pip install s3cmd",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
-      "sudo rm /usr/share/nginx/html/index.html",
-      "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
-      "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
-      "sudo logrotate -f /etc/logrotate.conf"
-
-    ]
-  }
-
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-nginx2" })
-
-}
-
-## S3 Bucket config ##
+# S3 Bucket config#
 resource "aws_iam_role" "allow_nginx_s3" {
   name = "allow_nginx_s3"
 
@@ -392,32 +356,52 @@ resource "aws_iam_role_policy" "allow_s3_all" {
   ]
 }
 EOF
-}
 
-resource "aws_s3_bucket" "web_bucket" {
-  bucket        = local.s3_bucket_name
-  acl           = "private"
-  force_destroy = true
+  }
 
-  tags = merge(local.common_tags, { Name = "${var.environment_tag}-web-bucket" })
-}
+  resource "aws_s3_bucket" "web_bucket" {
+    bucket        = local.s3_bucket_name
+    acl           = "private"
+    force_destroy = true
 
-resource "aws_s3_bucket_object" "website" {
-  bucket = aws_s3_bucket.web_bucket.bucket
-  key    = "/website/index.html"
-  source = "./index.html"
-}
+    tags = merge(local.common_tags, { Name = "${var.environment_tag}-web-bucket" })
 
-resource "aws_s3_bucket_object" "graphic" {
-  bucket = aws_s3_bucket.web_bucket.bucket
-  key    = "/website/Globo_logo_Vert.png"
-  source = "./Globo_logo_Vert.png"
-}
+  }
 
-##################################################################################
-# OUTPUT
-##################################################################################
+  resource "aws_s3_bucket_object" "website" {
+    bucket = aws_s3_bucket.web_bucket.bucket
+    key    = "/website/index.html"
+    source = "./index.html"
 
-output "aws_elb_public_dns" {
-  value = aws_elb.web.dns_name
-}
+  }
+
+  resource "aws_s3_bucket_object" "graphic" {
+    bucket = aws_s3_bucket.web_bucket.bucket
+    key    = "/website/Globo_logo_Vert.png"
+    source = "./Globo_logo_Vert.png"
+
+  }
+
+  # Azure RM DNS #
+  resource "azurerm_dns_cname_record" "elb" {
+    name                = "${var.environment_tag}-website"
+    zone_name           = var.dns_zone_name
+    resource_group_name = var.dns_resource_group
+    ttl                 = "30"
+    record              = aws_elb.web.dns_name
+    provider            = azurerm.arm-1
+
+    tags = merge(local.common_tags, { Name = "${var.environment_tag}-website" })
+  }
+
+  ##################################################################################
+  # OUTPUT
+  ##################################################################################
+
+  output "aws_elb_public_dns" {
+    value = aws_elb.web.dns_name
+  }
+
+  output "cname_record_url" {
+    value = "http://${var.environment_tag}-website.${var.dns_zone_name}"
+  }
